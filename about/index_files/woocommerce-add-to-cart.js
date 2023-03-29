09e904df9e2376070433a59b3312cce4d29e1ba6
captproton <carl@wdwhub.net>
2023-03-29 12:52:29 -0700

    first commit by upload: single files, /about, /ceremonies

---
+++ about/index_files/woocommerce-add-to-cart.js
@@ -0,0 +1,54 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+window.jQuery( document ).ready( function ( $ ) {
+	$( 'body' ).on( 'adding_to_cart', function ( event, $button, data ) {
+		$button && $button.hasClass( 'vc_gitem-link' ) && $button
+			.addClass( 'vc-gitem-add-to-cart-loading-btn' )
+			.parents( '.vc_grid-item-mini' )
+			.addClass( 'vc-woocommerce-add-to-cart-loading' )
+			.append( $( '<div class="vc_wc-load-add-to-loader-wrapper"><div class="vc_wc-load-add-to-loader"></div></div>' ) );
+	} ).on( 'added_to_cart', function ( event, fragments, cart_hash, $button ) {
+		if ( 'undefined' === typeof($button) ) {
+			$button = $( '.vc-gitem-add-to-cart-loading-btn' );
+		}
+		$button && $button.hasClass( 'vc_gitem-link' ) && $button
+			.removeClass( 'vc-gitem-add-to-cart-loading-btn' )
+			.parents( '.vc_grid-item-mini' )
+			.removeClass( 'vc-woocommerce-add-to-cart-loading' )
+			.find( '.vc_wc-load-add-to-loader-wrapper' ).remove();
+	} );
+} );
+
+
+}
+/*
+     FILE ARCHIVED ON 21:15:16 Aug 14, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 04:30:49 Mar 15, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 323.385
+  exclusion.robots: 0.062
+  exclusion.robots.policy: 0.054
+  cdx.remote: 0.047
+  esindex: 0.007
+  LoadShardBlock: 289.711 (3)
+  PetaboxLoader3.datanode: 95.433 (5)
+  PetaboxLoader3.resolve: 226.738 (3)
+  load_resource: 93.401
+  loaddict: 47.001
+*/
